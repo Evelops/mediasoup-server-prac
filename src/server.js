c13f7ee7cb0911b629e1568b4121698817ccc1e2
eomseung-yeol <co6747@naver.com>
2023-01-23 14:25:28 +0900

    chore:openssl 적용

--- src/server.js
+++ src/server.js
@@ -10,10 +10,13 @@ app.get('/',(req,res)=>{
     res.send('mediasoup app!');
 });
 
+// HTTPS & open ssl routes define.
 const options  = {
-    
+    key: fs.readFileSync('./ssl/key.pem','utf-8'),
+    cert: fs.readFileSync('./ssl/cert.pem','utf-8')
 };
 
+// run server
 const httpsServer = https.createServer(options, app);
 httpsServer.listen(3000,()=>{
     console.log('linsteing on port:'+3000);
